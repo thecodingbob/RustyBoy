55adcd367502ec5959e53c6f3b793bf49ed2d984
thecodingbob <vey90@live.it>
2023-12-29 16:31:02 +0100

    Completed 8 bit load instruction mapping

--- src/core/instructions/mapping.rs
+++ src/core/instructions/mapping.rs
@@ -22,126 +22,140 @@ impl Instruction {
 }
 
 const fn init_instruction_array() -> [Option<Instruction>; 256] {
-    let mut array = [None; 256];
+    let mut a = [None; 256];
     
-    array[0x02] = Some(LDBCA);
+    a[0x02] = Some(LDBCA);
     
-    array[0x06] = Some(LDRN(B));
+    a[0x06] = Some(LDRN(B));
     
-    array[0x0A] = Some(LDABC);
+    a[0x0A] = Some(LDABC);
     
-    array[0x0E] = Some(LDRN(C));
+    a[0x0E] = Some(LDRN(C));
     
-    array[0x12] = Some(LDDEA);
+    a[0x12] = Some(LDDEA);
     
-    array[0x16] = Some(LDRN(D));
+    a[0x16] = Some(LDRN(D));
     
-    array[0x1A] = Some(LDADE);
+    a[0x1A] = Some(LDADE);
     
-    array[0x1E] = Some(LDRN(E));
+    a[0x1E] = Some(LDRN(E));
     
-    array[0x22] = Some(LDHLINCA);
+    a[0x22] = Some(LDHLINCA);
     
-    array[0x26] = Some(LDRN(H));
+    a[0x26] = Some(LDRN(H));
     
-    array[0x2A] = Some(LDAHLINC);
+    a[0x2A] = Some(LDAHLINC);
     
-    array[0x2E] = Some(LDRN(L));
+    a[0x2E] = Some(LDRN(L));
     
-    array[0x32] = Some(LDHLDECA);
+    a[0x32] = Some(LDHLDECA);
     
-    array[0x36] = Some(LDHLN);
+    a[0x36] = Some(LDHLN);
     
-    array[0x3A] = Some(LDAHLDEC);
+    a[0x3A] = Some(LDAHLDEC);
     
-    array[0x3E] = Some(LDRN(A));
+    a[0x3E] = Some(LDRN(A));
     
-    array[0x40] = Some(LDRR(B,B));
-    array[0x41] = Some(LDRR(B,C));
-    array[0x42] = Some(LDRR(B,D));
-    array[0x43] = Some(LDRR(B,E));
-    array[0x44] = Some(LDRR(B,H));
-    array[0x45] = Some(LDRR(B,L));
-    array[0x46] = Some(LDRHL(B));
-    array[0x47] = Some(LDRR(B,A));
-    array[0x48] = Some(LDRR(C,B));
-    array[0x49] = Some(LDRR(C,C));
-    array[0x4A] = Some(LDRR(C,D));
-    array[0x4B] = Some(LDRR(C,E));
-    array[0x4C] = Some(LDRR(C,H));
-    array[0x4D] = Some(LDRR(C,L));
-    array[0x4E] = Some(LDRHL(C));
-    array[0x4F] = Some(LDRR(C,A));
-    array[0x50] = Some(LDRR(D,B));
-    array[0x51] = Some(LDRR(D,C));
-    array[0x52] = Some(LDRR(D,D));
-    array[0x53] = Some(LDRR(D,E));
-    array[0x54] = Some(LDRR(D,H));
-    array[0x55] = Some(LDRR(D,L));
-    array[0x56] = Some(LDRHL(D));
-    array[0x57] = Some(LDRR(D,A));
-    array[0x58] = Some(LDRR(E,B));
-    array[0x59] = Some(LDRR(E,C));
-    array[0x5A] = Some(LDRR(E,D));
-    array[0x5B] = Some(LDRR(E,E));
-    array[0x5C] = Some(LDRR(E,H));
-    array[0x5D] = Some(LDRR(E,L));
-    array[0x5E] = Some(LDRHL(E));
-    array[0x5F] = Some(LDRR(E,A));
-    array[0x60] = Some(LDRR(H,B));
-    array[0x61] = Some(LDRR(H,C));
-    array[0x62] = Some(LDRR(H,D));
-    array[0x63] = Some(LDRR(H,E));
-    array[0x64] = Some(LDRR(H,H));
-    array[0x65] = Some(LDRR(H,L));
-    array[0x66] = Some(LDRHL(H));
-    array[0x67] = Some(LDRR(H,A));
-    array[0x68] = Some(LDRR(L,B));
-    array[0x69] = Some(LDRR(L,C));
-    array[0x6A] = Some(LDRR(L,D));
-    array[0x6B] = Some(LDRR(L,E));
-    array[0x6C] = Some(LDRR(L,H));
-    array[0x6D] = Some(LDRR(L,L));
-    array[0x6E] = Some(LDRHL(L));
-    array[0x6F] = Some(LDRR(L,A));
-    array[0x70] = Some(LDHLR(B));
-    array[0x71] = Some(LDHLR(C));
-    array[0x72] = Some(LDHLR(D));
-    array[0x73] = Some(LDHLR(E));
-    array[0x74] = Some(LDHLR(H));
-    array[0x75] = Some(LDHLR(L));
+    a[0x40] = Some(LDRR(B, B));
+    a[0x41] = Some(LDRR(B, C));
+    a[0x42] = Some(LDRR(B, D));
+    a[0x43] = Some(LDRR(B, E));
+    a[0x44] = Some(LDRR(B, H));
+    a[0x45] = Some(LDRR(B, L));
+    a[0x46] = Some(LDRHL(B));
+    a[0x47] = Some(LDRR(B, A));
+    a[0x48] = Some(LDRR(C, B));
+    a[0x49] = Some(LDRR(C, C));
+    a[0x4A] = Some(LDRR(C, D));
+    a[0x4B] = Some(LDRR(C, E));
+    a[0x4C] = Some(LDRR(C, H));
+    a[0x4D] = Some(LDRR(C, L));
+    a[0x4E] = Some(LDRHL(C));
+    a[0x4F] = Some(LDRR(C, A));
+    a[0x50] = Some(LDRR(D, B));
+    a[0x51] = Some(LDRR(D, C));
+    a[0x52] = Some(LDRR(D, D));
+    a[0x53] = Some(LDRR(D, E));
+    a[0x54] = Some(LDRR(D, H));
+    a[0x55] = Some(LDRR(D, L));
+    a[0x56] = Some(LDRHL(D));
+    a[0x57] = Some(LDRR(D, A));
+    a[0x58] = Some(LDRR(E, B));
+    a[0x59] = Some(LDRR(E, C));
+    a[0x5A] = Some(LDRR(E, D));
+    a[0x5B] = Some(LDRR(E, E));
+    a[0x5C] = Some(LDRR(E, H));
+    a[0x5D] = Some(LDRR(E, L));
+    a[0x5E] = Some(LDRHL(E));
+    a[0x5F] = Some(LDRR(E, A));
+    a[0x60] = Some(LDRR(H, B));
+    a[0x61] = Some(LDRR(H, C));
+    a[0x62] = Some(LDRR(H, D));
+    a[0x63] = Some(LDRR(H, E));
+    a[0x64] = Some(LDRR(H, H));
+    a[0x65] = Some(LDRR(H, L));
+    a[0x66] = Some(LDRHL(H));
+    a[0x67] = Some(LDRR(H, A));
+    a[0x68] = Some(LDRR(L, B));
+    a[0x69] = Some(LDRR(L, C));
+    a[0x6A] = Some(LDRR(L, D));
+    a[0x6B] = Some(LDRR(L, E));
+    a[0x6C] = Some(LDRR(L, H));
+    a[0x6D] = Some(LDRR(L, L));
+    a[0x6E] = Some(LDRHL(L));
+    a[0x6F] = Some(LDRR(L, A));
+    a[0x70] = Some(LDHLR(B));
+    a[0x71] = Some(LDHLR(C));
+    a[0x72] = Some(LDHLR(D));
+    a[0x73] = Some(LDHLR(E));
+    a[0x74] = Some(LDHLR(H));
+    a[0x75] = Some(LDHLR(L));
     
-    array[0x77] = Some(LDHLR(A));
-    array[0x78] = Some(LDRR(A,B));
-    array[0x79] = Some(LDRR(A,C));
-    array[0x7A] = Some(LDRR(A,D));
-    array[0x7B] = Some(LDRR(A,E));
-    array[0x7C] = Some(LDRR(A,H));
-    array[0x7D] = Some(LDRR(A,L));
-    array[0x7E] = Some(LDRHL(A));
-    array[0x7F] = Some(LDRR(A,A));
-    array[0x80] = Some(ADDR(B));
-    array[0x81] = Some(ADDR(C));
-    array[0x82] = Some(ADDR(D));
-    array[0x83] = Some(ADDR(E));
-    array[0x84] = Some(ADDR(H));
-    array[0x85] = Some(ADDR(L));
-    array[0x86] = Some(ADDHL);
-    array[0x87] = Some(ADDR(A));
-    array[0x88] = Some(ADCR(B));
-    array[0x89] = Some(ADCR(C));
-    array[0x8A] = Some(ADCR(D));
-    array[0x8B] = Some(ADCR(E));
-    array[0x8C] = Some(ADCR(H));
-    array[0x8D] = Some(ADCR(L));
-    array[0x8E] = Some(ADCHL);
-    array[0x8F] = Some(ADCR(A));
+    a[0x77] = Some(LDHLR(A));
+    a[0x78] = Some(LDRR(A, B));
+    a[0x79] = Some(LDRR(A, C));
+    a[0x7A] = Some(LDRR(A, D));
+    a[0x7B] = Some(LDRR(A, E));
+    a[0x7C] = Some(LDRR(A, H));
+    a[0x7D] = Some(LDRR(A, L));
+    a[0x7E] = Some(LDRHL(A));
+    a[0x7F] = Some(LDRR(A, A));
+    a[0x80] = Some(ADDR(B));
+    a[0x81] = Some(ADDR(C));
+    a[0x82] = Some(ADDR(D));
+    a[0x83] = Some(ADDR(E));
+    a[0x84] = Some(ADDR(H));
+    a[0x85] = Some(ADDR(L));
+    a[0x86] = Some(ADDHL);
+    a[0x87] = Some(ADDR(A));
+    a[0x88] = Some(ADCR(B));
+    a[0x89] = Some(ADCR(C));
+    a[0x8A] = Some(ADCR(D));
+    a[0x8B] = Some(ADCR(E));
+    a[0x8C] = Some(ADCR(H));
+    a[0x8D] = Some(ADCR(L));
+    a[0x8E] = Some(ADCHL);
+    a[0x8F] = Some(ADCR(A));
 
-    array[0xC6] = Some(ADDN);
+    a[0xC6] = Some(ADDN);
 
-    array[0xCE] = Some(ADCN);
+    a[0xCE] = Some(ADCN);
 
-    array
+    a[0xE0] = Some(LDHNA);
+
+    a[0xE2] = Some(LDHCA);
+    // None
+    // None
+
+    a[0xEA] = Some(LDNNA);
+
+    a[0xF0] = Some(LDHAN);
+
+    a[0xF2] = Some(LDHAC);
+
+    a[0xFA] = Some(LDANN);
+
+    a
 }
 
 const fn init_prefix_instruction_array() -> [Option<Instruction>; 256] {
